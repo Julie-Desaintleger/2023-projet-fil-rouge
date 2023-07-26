5df3115af68d620cf572cd60ef1da1c6c9a23aed
Julie Desaintléger <julie.desaintleger@gmail.com>
2023-07-26 16:36:07 +0200

    Init project

---
+++ AppBibliotheque/src/main/java/com/inetum/AppBibliotheque/AppBibliothequeApplication.java
@@ -0,0 +1,13 @@
+package com.inetum.AppBibliotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBibliothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBibliothequeApplication.class, args);
+	}
+
+}
---
+++ AppBibliotheque/src/test/java/com/inetum/AppBibliotheque/AppBibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inetum.AppBibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBibliothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
