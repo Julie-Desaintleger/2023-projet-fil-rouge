653db1eff50d770ba898d0fa2a8511d61247f2df
Anouar EL KADIOUI EL IDRISSI <anouarelidrissi1996@gmail.com>
2023-08-30 16:45:18 +0200

    livre ajax

--- AppBibliotheque/src/main/resources/static/livreAjax.js
+++ AppBibliotheque/src/main/resources/static/livreAjax.js
@@ -1,4 +1,4 @@
-function aficherLivres(){
+/*function aficherLivres(){
 	
 	let wsUrl = "./api-livres/livre";
 	
@@ -17,4 +17,24 @@ function aficherLivres(){
 		}
 	});
 	
+}*/
+
+// Create an XMLHttpRequest object
+var xmlhttp = new XMLHttpRequest()
+// Define a callback function
+xmlhttp.onload = function() {
+let data = JSON.parse(this.responseText)
+//	console.log(data)
+		let bodyElt = document.getElementById("table_body");
+		bodyElt.innerHTML="";//vider le tableau avant de le re-remplir
+			for(let x of data){
+				let row = bodyElt.insertRow(-1);
+				(row.insertCell(0)).innerHTML = x.idLivre;
+				(row.insertCell(1)).innerHTML = x.auteur;
+				(row.insertCell(2)).innerHTML = x.editeur;
+				(row.insertCell(3)).innerHTML = x.titre;
+			}
 }
+// Send a request
+xmlhttp.open("GET", "http://localhost:8080/AppBibliotheque/api-livres/livre")
+xmlhttp.send()
