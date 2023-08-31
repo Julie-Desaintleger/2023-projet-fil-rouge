da7c5f2c90247b063d205c63c3598189e76a07c8
Anouar EL KADIOUI EL IDRISSI <anouarelidrissi1996@gmail.com>
2023-08-31 13:50:25 +0200

    fichier html modifié

--- AppBibliotheque/src/main/resources/static/livreAjax.js
+++ AppBibliotheque/src/main/resources/static/livreAjax.js
@@ -1,23 +1,4 @@
-/*function aficherLivres(){
-	
-	let wsUrl = "./api-livres/livre";
-	
-	makeAjaxGetRequest(wsUrl,function(responseJson){
-		let livresJs = JSON.parse(responseJson);
-		console.log("livresJs="+livresJs);
-		
-		let bodyElt = document.getElementById("table_body");
-		bodyElt.innerHTML="";//vider le tableau avant de le re-remplir
-		for(let livre of livresJs){
-			let row = bodyElt.insertRow(-1);
-			(row.insertCell(0)).innerHTML = livre.Reference;
-			(row.insertCell(1)).innerHTML = livre.Auteur;
-			(row.insertCell(2)).innerHTML = livre.Editeur;
-			(row.insertCell(3)).innerHTML = livre.Titre;
-		}
-	});
-	
-}*/
+
 
 // Create an XMLHttpRequest object
 var xmlhttp = new XMLHttpRequest()
